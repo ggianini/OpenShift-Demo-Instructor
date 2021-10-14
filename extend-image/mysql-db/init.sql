2e28d3e482fda6b8744279cae7db1758866ad156
Hugo Pfeffer <hugo.pguima@gmail.com>
2021-10-14 18:53:10 -0300

    fix: extend-image layout

--- extend-image/mysql-db/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE cidade (id INT NOT NULL, nome VARCHAR(50) NOT NULL, PRIMARY KEY (id));
-
-
-INSERT INTO cidade (id,nome) VALUES(1,"Rio de Janeiro");
-INSERT INTO cidade (id,nome) VALUES(2,"Brasilia");
-INSERT INTO cidade (id,nome) VALUES(3,"Vitoria");
